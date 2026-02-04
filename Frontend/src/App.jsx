25ae5142a6e6dc442ae23e79716e10831411e60d
Saurravv <cactussaurav@gmail.com>
2026-02-04 10:54:59 +0545

    Feat: all Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Enroll from "./pages/Enroll";
+import Follow from "./pages/Follow";
+import Footer from "./pages/Footer";
+import Gallery from "./pages/Gallery";
+import Home from "./pages/Home";
+import LatestNews from "./pages/LatestNews";
+import Notice from "./pages/Notice";
+import Program from "./pages/Program";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Enroll" element={<Enroll />} />
+        <Route path="/Follow" element={<Follow />} />
+        <Route path="/Foooter" element={<Footer />} />
+        <Route path="/Latest" element={<LatestNews />} />
+        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="/Notice" element={<Notice />} />
+        <Route path="/Program" element={<Program />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
